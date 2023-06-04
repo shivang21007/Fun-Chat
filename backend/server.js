8fcc06ed2837ea7934edace1be5d344c8bcb532d
Shivang21007 <shivanggupta2611@gmail.com>
2023-06-05 02:44:17 +0530

    backend added

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("What is Socket: ", socket);
+  console.log("Socket is active to be connected");
+
+  socket.on("chat", (payload) => {
+    console.log("what is payload", payload);
+    io.emit("chat", payload);
+  });
+});
+
+//app.listen(5000, () => console.log("Server is active..."));
+
+server.listen(8000, () => console.log("Server is active at 8000..."));
