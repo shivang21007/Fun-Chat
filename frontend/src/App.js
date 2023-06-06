184a8a266e01c7e91455bcd693667575bbe9836f
Shivang21007 <shivanggupta2611@gmail.com>
2023-06-06 16:13:19 +0530

    nodemon uninstalled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,8 @@ import io from "socket.io-client";
 import { nanoid } from "nanoid";
 
 // no dotenv
-const socket = io.connect("http://192.168.0.111:8000"); //if you want to run-multiple devices
-//const socket = io.connect("http://localhost:8000");
+//const socket = io.connect("http://192.168.0.100:8000"); //if you want to run-multiple devices
+const socket = io.connect("http://localhost:8000");
 
 //User Name Assign
 const userName = nanoid(3);
